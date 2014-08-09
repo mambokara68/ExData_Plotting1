7a3f89c2b76992ce944a72883839c8eb65ae75c8
mambokara68 <mambokara68@users.noreply.github.com>
2014-08-09 21:08:24 +0200

    Update Plot4.R

--- Myscripts/Plot4.R
+++ Myscripts/Plot4.R
@@ -13,16 +13,16 @@ png(filename = "Plot4.png",width = 480, height = 480)
 ## 4 graphs on the page, 2 rows 2columns
 par(mfrow = c(2, 2))
 ##Plot the 4 needed graphs
-##same as Plot1
-plot(data$datetime,data$Global_active_power,type="l",xlab="",ylab="Global Active Power")
 ##same as Plot2
+plot(data$datetime,data$Global_active_power,type="l",xlab="",ylab="Global Active Power")
+##a new one
 plot(data$datetime,data$Voltage,type="l",xlab="datetime",ylab="Voltage")
 ##same as Plot3
 plot(data$datetime,data$Sub_metering1,type="l",xlab="",ylab="Energy sub metering")
 lines(data$datetime,data$Sub_metering2,type="l",xlab="",ylab="Energy sub metering",col="red")
 lines(data$datetime,data$Sub_metering3,type="l",xlab="",ylab="Energy sub metering",col="blue")
 legend("topright",cex=.75, bty="n",lty=1, col = c("black","red", "blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
-## the new one
+## a new one
 plot(data$datetime,data$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
 ## Close the acquisition
 dev.off()
